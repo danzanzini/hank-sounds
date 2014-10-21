ecdf2bd2e1032bc50d1e9103c443b6f804b5d028
Daniel Zanzini <zanzini@mitahtech.com>
2014-10-21 16:01:49 -0200

    Correção para finalizar remoção de butão de settings

--- app/src/main/java/com/zanzinidev/hanksounds/ButtonsActivity.java
+++ app/src/main/java/com/zanzinidev/hanksounds/ButtonsActivity.java
@@ -41,18 +41,6 @@ public class ButtonsActivity extends ActionBarActivity {
         });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     private void playSound(int sound){
 
         MediaPlayer mp = MediaPlayer.create(this, sound);
